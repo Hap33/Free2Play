8cc553442293051fc4a91e7a35f76290850e8b8d
Axel <axeldeconti@gmail.com>
2018-10-09 08:55:34 +0200

    scene Sound_Test

---
+++ Assets/Scripts/Sound_Test.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class Sound_Test : MonoBehaviour {
+
+    public AudioSource audioSource;
+
+    //Sets the pitch to the value in parameter
+    public void SetPitch(float _pitch)
+    {
+        audioSource.pitch = _pitch;
+    }
+}
