2903ac1091cc8cd90a3b6a6fc1178b18a19a6abf
Hap33 <36961317+Hap33@users.noreply.github.com>
2018-10-23 16:38:18 +0200

    MAJ Music

---
+++ Assets/Scripts/MusicScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicScript : MonoBehaviour {
+
+    public static MusicScript instance;
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if (SceneManager.GetActiveScene().buildIndex > 3)
+        {
+            Destroy(gameObject);
+        }
+	}
+}
