b51ec534cdf9e79f19469bbf7422ebe8f42ef6ba
Gaspard BROUSSAUD <gaspard.broussaud1999@gmail.com>
2018-10-25 09:25:38 +0200

    Fix Menu Pause Level 1

--- Assets/Scripts/MainMenuScript.cs
+++ Assets/Scripts/MainMenuScript.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Level_1 : MonoBehaviour {
+public class MainMenuScript : MonoBehaviour {
 
     public void SceneLaunch(string nomScene)
     {
