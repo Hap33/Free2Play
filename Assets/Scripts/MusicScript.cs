4193a7885d6ab2ab7c0d74607e191f56cf40ff52
Gaspard BROUSSAUD <gaspard.broussaud1999@gmail.com>
2018-10-25 16:18:38 +0200

    BIG push

--- Assets/Scripts/MusicScript.cs
+++ Assets/Scripts/MusicScript.cs
@@ -21,7 +21,7 @@ public class MusicScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (SceneManager.GetActiveScene().buildIndex > 3)
+		if (SceneManager.GetActiveScene().buildIndex > 4)
         {
             Destroy(gameObject);
         }
